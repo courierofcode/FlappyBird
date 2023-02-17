545890c12a9bc747841e0525792e5a963a1cb3de
courierofcode <notifyemmanuel@gmail.com>
2023-02-17 01:30:07 -0600

    Initial Commit

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    public GameObject gameStartScreen;
+
+    [ContextMenu("Add Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore += scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    public void startGame()
+    {
+        Time.timeScale = 1f;
+        scoreText.text = "0";
+        gameStartScreen.SetActive(false);
+    }
+}
